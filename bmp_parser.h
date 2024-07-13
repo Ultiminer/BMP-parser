bfad6032cdb48c472611e298ac615affa172d86f
FloDev <43553244+Ultiminer@users.noreply.github.com>
2024-07-13 19:28:36 +0200

    Initial commit

---
+++ bmp_parser.h
@@ -0,0 +1,10 @@
+#ifndef BMP_PARSER_H_
+#define BMP_PARSER_H_
+
+namespace BMP
+{
+    
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "bmp_parser.h"
+
+
+
+int main(int argc, char** argv)
+{
+
+    
+    return 0;
+}
